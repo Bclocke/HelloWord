7bdea62f3fefd7baf339d76b8715615e7175e16a
Brandon Locke <bclocke@crimson.ua.edu>
2014-09-30 15:23:18 -0500

    HelloWorldApp
    Test App

---
+++ HelloWord/app/controllers/index.js
@@ -0,0 +1,14 @@
+var myText = "BOOM, YOU ARE DEAD!";
+var myText2 = "YOU ARE OKAY!";
+
+function doClick(e) {
+    alert(myText);
+}
+
+function doClick2(e) {
+    alert(myText2);
+}
+
+
+
+$.index.open();
